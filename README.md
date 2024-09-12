a4df1ddd894c0fad4ea162402705396773da1dd9
James Bradley <jamesbradleym@gmail.com>
2024-09-12 18:52:58 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+
+
+# searchable
+
+The Searchable function.
+
+|Input Name|Type|Description|
+|---|---|---|
+|Level count|integer||
+|Level Names|[
+  "string",
+  "null"
+]||
+|Searchable Level Names|[
+  "string",
+  "null"
+]||
+|Choices|string||
+|Searchable Choices|string||
+
+
+<br>
+
+|Output Name|Type|Description|
+|---|---|---|
+
+
+<br>
+
+## Additional Information
---
+++ src/Searchable.cs
@@ -0,0 +1,38 @@
+using Elements;
+using Elements.Geometry;
+using System.Collections.Generic;
+
+namespace Searchable
+{
+  public static class Searchable
+  {
+    /// <summary>
+    /// The Searchable function.
+    /// </summary>
+    /// <param name="model">The input model.</param>
+    /// <param name="input">The arguments to the execution.</param>
+    /// <returns>A SearchableOutputs instance containing computed results and the model with any new elements.</returns>
+    public static SearchableOutputs Execute(Dictionary<string, Model> inputModels, SearchableInputs input)
+    {
+      // Your code here.
+      var output = new SearchableOutputs();
+
+      // Gather inputs.
+      var choices = input.Choices;
+
+      foreach (var elevation in Enumerable.Range(0, input.LevelCount))
+      {
+        var level = new Level
+        {
+          Elevation = elevation * 3,
+          Height = 3,
+          Name = $"Level {elevation}"
+        };
+
+        output.Model.AddElement(level);
+      }
+
+      return output;
+    }
+  }
+}
